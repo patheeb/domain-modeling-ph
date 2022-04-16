0aa668068b48de441b1f2b1def9b73e9db4802f3
patheeb <pat.heeb@kellogg.northwestern.edu>
2022-04-16 14:14:20 +0000

    Inclass practice on domain modeling

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,40 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+DROP TABLE IF EXISTS company_list;
+DROP TABLE IF EXISTS contact_list;
+DROP TABLE IF EXISTS activity_list;
+DROP TABLE IF EXISTS salesperson_list;
+
+
+CREATE TABLE salesperson_list (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT
+
+)
+
+CREATE TABLE company_list (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_name TEXT
+);
+
+CREATE TABLE contact_list (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_list_id INTEGER
+);
+
+CREATE TABLE activity_list (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    salespersonlist_id INTEGER,
+    contactlist_id INTEGER,
+    companylist_id INTEGER,
+    contact_type TEXT,
+    time TEXT,
+    description TEXT
+);
--- school/school.sql
+++ school/school.sql
@@ -1,10 +1,27 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
-DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS teachers;
-DROP TABLE IF EXISTS courses;
-DROP TABLE IF EXISTS sections;
-DROP TABLE IF EXISTS enrollments;
+CREATE TABLE company_list (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_name TEXT
+);
+
+CREATE TABLE contact_list (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  companylist_id INTEGER
+);
+
+CREATE TABLE activity_list (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    contactlist_id INTEGER,
+    companylist_id INTEGER,
+    contact_type TEXT,
+    time TEXT,
+    description TEXT
+);
 
 CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,4 +33,22 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  course_name TEXT,
+  course_description TEXT
+);
+
+CREATE Table sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  couse_time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
